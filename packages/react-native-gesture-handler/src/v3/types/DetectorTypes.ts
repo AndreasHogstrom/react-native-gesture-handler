957eed3ed92806dd77d1bd6382ab20a8c6869d14
Michał Bert <63123542+m-bert@users.noreply.github.com>
2026-02-26 14:52:26 +0100

    Split `HandlerData` (#3980)
    ## Description
    
    This PR splits `HandlerData` type into `HandlerData` and
    `ExtendedHandlerData`. This allows us to hide `change*` properties from
    callbacks that shouldn't have them in their `event` parameter. It also
    removes `anchor` and `focal` from `rotation` and `pinch` from `onBegin`
    and `onFinalize` callbacks.
    
    ## Test plan
    
    - `yarn ts-check`
    - `yarn lint-js`
    
    
    <details>
    <summary>Test code</summary>
    
    I've not attached gestures as this is type only change. However, they
    were tested on **expo-example**.
    
    ```tsx
    import React from 'react';
    import { View } from 'react-native';
    import { usePanGesture, usePinchGesture } from 'react-native-gesture-handler';
    
    export default function EmptyExample() {
      const pan = usePanGesture({
        onBegin: (e) => {
          console.log('pan begin', e.translationX);
        },
        onUpdate: (e) => {
          console.log('pan update', e.changeX);
        },
      });
    
      const pinch = usePinchGesture({
        onBegin: (e) => {
          console.log('pinch begin', e.scale, e);
        },
        onUpdate: (e) => {
          console.log('pinch update', e.scaleChange, e.focalX);
        },
      });
    
      console.log(pan, pinch);
    
      return <View />;
    }
    ```
    
    </details>

--- packages/react-native-gesture-handler/src/v3/types/DetectorTypes.ts
+++ packages/react-native-gesture-handler/src/v3/types/DetectorTypes.ts
@@ -5,23 +5,38 @@ import {
 } from './EventTypes';
 import { TouchAction, UserSelect } from '../../handlers/gestureHandlerCommon';
 
-export type DetectorCallbacks<THandlerData> = {
+export type DetectorCallbacks<
+  THandlerData,
+  TExtendedHandlerData extends THandlerData = THandlerData,
+> = {
   jsEventHandler:
     | undefined
-    | ((event: GestureHandlerEventWithHandlerData<THandlerData>) => void);
+    | ((
+        event: GestureHandlerEventWithHandlerData<
+          THandlerData,
+          TExtendedHandlerData
+        >
+      ) => void);
   reanimatedEventHandler:
     | undefined
-    | ((event: GestureHandlerEventWithHandlerData<THandlerData>) => void);
+    | ((
+        event: GestureHandlerEventWithHandlerData<
+          THandlerData,
+          TExtendedHandlerData
+        >
+      ) => void);
   animatedEventHandler:
     | undefined
     | AnimatedEvent
-    | ((event: GestureUpdateEventWithHandlerData<THandlerData>) => void);
+    | ((
+        event: GestureUpdateEventWithHandlerData<TExtendedHandlerData>
+      ) => void);
 };
 
 export type VirtualChild = {
   viewTag: number;
   handlerTags: number[];
-  methods: DetectorCallbacks<unknown>;
+  methods: DetectorCallbacks<unknown, unknown>;
 
   // only set on web
   viewRef: unknown;
